e1391017d016d613f063f3a5b5ab3073e34ebed7
Pomidor4ik <onereg4all@mail.ru>
2018-10-16 09:05:22 +0900

    Изменился адрес личного кабинета

--- providers/ab-internet-tele-vox/main.js
+++ providers/ab-internet-tele-vox/main.js
@@ -19,7 +19,7 @@ function main(){
 
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "http://www.tele-vox.ru/webexecuter";
+    var baseurl = "http://www.tele-vox.ru/";
 
     var html = AnyBalance.requestPost(baseurl, {
         midAuth:0,
